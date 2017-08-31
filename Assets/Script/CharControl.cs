31d547aa9ac268235de930be576e81c8dadffc12
jkgman <jgranger2009@gmail.com>
2017-08-31 13:41:49 +0300

    Added Spaceship

---
+++ Assets/Script/CharControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class CharControl : MonoBehaviour
+    {
+        public float speed = 3f;
+        private Vector3 getMovementVector() {
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            return new Vector3(x,y,0);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Translate(getMovementVector()* Time.deltaTime * speed);
+        }
+    }
+}
